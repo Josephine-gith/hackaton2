0ddd1bad2f2bf3086528bab027eed83dbd5fb5fd
Luigiromain <126761248+Luigiromain@users.noreply.github.com>
2025-01-31 11:12:52 +0100

    chrono

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import classes
 from ines import *
 from josephine import *
+from pynput import keyboard
+
+
+while True:
+    """
+    objectif général :
+    move(player)
+    analyser_case (recup les objets)
+    if combat :
+        combat
+    else :
+        move(entities)
+    update(map)
+    display(map)"""

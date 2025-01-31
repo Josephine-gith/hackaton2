a129c7157fc74d2cf48836aaa95a2c0b1852fddc
m-ines <inesmmimouni@gmail.com>
2025-01-31 15:30:30 +0100

    ajout de caracteristiques

--- classes.py
+++ classes.py
@@ -1,9 +1,10 @@
 class Entity:
-    def __init__(self, x, y, name, car):
+    def __init__(self, x, y, name, car, life):
         self.x = x
         self.y = y
         self.name = name
         self.car = car
+        self.life = life
 
     def move(self, nextmove, map):
         if nextmove == "s":
@@ -25,7 +26,7 @@ class Entity:
             else:
                 return self.x, self.y - 1
         elif nextmove == "d":
-            if is_empty(self.x, self.y + 1, map):
+            if is_empty(self.x, self.y + 1, map) :
                 self.y += 1
                 return self.x, self.y
             else:
@@ -37,8 +38,16 @@ class Entity:
 
 class Player(Entity):
     def __init__(self, x, y, name):
-        super().__init__(x, y, name, "@")
-
+        super().__init__(x, y, name, "@", 16)
+        self.level = 1
+        self.hits = 15
+        self.defense = 5
+        self.attack = 0 
+        self.hunger = 100 # chaque 100 pas font perdre 1 point de faim
+        self.thirst = 100 # chaque 20 pas font perdre un point de soif
+        self.inventory = {"gold" : 0 ,
+                          "potions" : 0 
+                          } # inventory est un dictionnaire key = objet, value = combien on en a
 
 class Valroy(Entity):
     def __init__(self, x, y, name):

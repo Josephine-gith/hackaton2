70b8c1967f589adeffb6ced5a03961631aed2677
Josephine-gith <josephine.peronne@gmail.com>
2025-01-31 13:39:29 +0100

    hassoul

--- josephine.py
+++ josephine.py
@@ -1,8 +1,33 @@
-from classes import Player
+# classes
+class Player:
+    def __init__(self, x, y, name):
+        self.x = x
+        self.y = y
+        self.name = name
+        self.car = '@'
+
+    def move(self, nextmove):
+        if nextmove == "d":
+            self.x += 1
+        elif nextmove == "q":
+            self.x -= 1
+        elif nextmove == "z":
+            self.y -= 1
+        elif nextmove == "s":
+            self.y += 1
+    
+    def __repr__(self):
+        return f"{self.name} : ({self.x, self.y})"
 
 #fonctions utiles
 def print_map(map): 
     print(''.join([''.join(map[k]) for k in range (len(map))]))
+
+def discover(player, amap, file):
+    x=player.x
+    y=player.y
+    for i,j in {(x-1,y-1),(x-1,y),(x-1,y+1),(x,y-1),(x,y),(x,y+1),(x+1,y-1),(x+1,y),(x+1,y+1)}:
+        amap[j][i]=file[j][i]
 
 def update_map(player,move,map):
     with open('josephine.txt','r') as in_file:
@@ -22,7 +47,10 @@ with open('josephine.txt', 'r') as in_file:
 #name=input('Quel est ton nom ? ')
 P = Player(1,1,'rogue')
 file[P.y][P.x]='@'
-print_map(file)
+amap = [([' ' for j in range(len(file[0])-1)]+['\n']) for i in range(len(file))]
+discover(P, amap, file)
+print_map(amap)
+
 
 #jeu en cours
 nextmove=None
@@ -30,9 +58,5 @@ while nextmove!='t':
     nextmove=input()
     if nextmove in {'q','s','z','d'}:
         update_map(P,nextmove,file)
-        print_map(file)
-    
-
-
-
-
+        discover(P,amap,file)
+        print_map(amap)

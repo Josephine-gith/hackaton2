aae7d1ecfee9716fa9de2ee045e20c705bd1c1b4
Josephine-gith <josephine.peronne@gmail.com>
2025-01-31 11:43:20 +0100

    avant pause

--- josephine.py
+++ josephine.py
@@ -1,28 +1,32 @@
 from classes import Player as pl
 
+
 #fonctions utiles
-def show(map):
-    for line in map:
-        for c in line:
-            print(c)
+def print_map(map) : 
+    print(''.join([''.join(map[k]) for k in range (len(map))]))
 
 #inplémentation de la map
 with open('josephine.txt', 'r') as in_file:
     file = in_file.readlines()
-    for i in range(len(file)):
-        file[i] = list(file[i])
+    file = [list(line) for line in file]
 
 ##déroulé du jeu
 
 #initialisation
 #name=input('Quel est ton nom ? ')
 P = pl(1,1,'rogue')
-show(file)
+file[P.x][P.y]='@'
+print_map(file)
 
+#jeu en cours
+nextmove=None
+while nextmove!='t':
+    nextmove=input()
+    if nextmove in {'q','s','z','d'}:
+        P.move(nextmove)
 
-    #nextmove = input()
-    #while nextmove != 't':
-        #P.move(nextmove)
+    
+
 
 
 

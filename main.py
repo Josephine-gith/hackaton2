d58efcda530e25899d915073af0b277e19f6fd70
aurelien-bonin <aurelien.bonin@etu.minesparis.psl.eu>
2025-01-31 10:47:08 +0100

    add health

---
+++ main.py
@@ -0,0 +1,16 @@
+class Player:
+    def __init__(self, x, y, name):
+        self.x = x
+        self.y = y
+        self.name = name
+        self.health = 10
+
+    def move(self, nextmove):
+        if nextmove == "d":
+            self.x += 1
+        elif nextmove == "q":
+            self.x -= 1
+        elif nextmove == "z":
+            self.y += 1
+        elif nextmove == "s":
+            self.y -= 1

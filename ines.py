c215268c00a2ee907bb80f5750040ad9f27212b5
m-ines <inesmmimouni@gmail.com>
2025-01-31 13:35:05 +0100

    tentative de sauvetage de mes donnees, merci luigi

--- ines.py
+++ ines.py
@@ -51,7 +51,45 @@ while nextmove!='t':
     if nextmove in {'q','s','z','d'}:
         refresh(pl,nextmove)
 
+class Entity():
+    def __init__(self, x, y, name, car):
+        self.x = x
+        self.y = y
+        self.name = name
+        self.car = car
 
+    def move(self, nextmove):
+        if nextmove == "d":
+            if is_empty(self.x+1,self.y):
+                self.x += 1
+                return self.x, self.y
+            else:
+                return self.x+1, self.y
+        elif nextmove == "q":
+            if is_empty(self.x-1,self.y):
+                self.x -= 1
+                return self.x,self.y
+            else:
+                return self.x-1, self.y
+        elif nextmove == "z":
+            if is_empty(self.x,self.y-1):
+                self.y -= 1
+                return self.x,self.y
+            else:
+                return self.x, self.y-1
+        elif nextmove == "s":
+            if is_empty(self.x,self.y-1):
+                self.y += 1
+                return self.x,self.y
+            else:
+                return self.x, self.y+1
+    
+    def __repr__(self):
+        return f"{self.name} : ({self.x, self.y})"
+    
+class Player(Entity) : 
+    def __init__(self, x, y, name):
+        super().__init__(x,y,name,'@')
 
 
     

04fee19aece1ffb327122a2debeb7233b2124598
Luigiromain <126761248+Luigiromain@users.noreply.github.com>
2025-01-31 13:26:27 +0100

    poulet braisé

--- classes.py
+++ classes.py
@@ -3,38 +3,40 @@ class Player:
         self.x = x
         self.y = y
         self.name = name
-        self.car = '@'
+        self.car = "@"
 
-    def move(self, nextmove):
+    def move(self, nextmove, map):
         if nextmove == "d":
-            if is_empty(self.x+1,self.y):
+            if is_empty(self.x + 1, self.y, map):
                 self.x += 1
                 return self.x, self.y
             else:
-                return self.x+1, self.y
+                return self.x + 1, self.y
         elif nextmove == "q":
-            if is_empty(self.x-1,self.y):
+            if is_empty(self.x - 1, self.y, map):
                 self.x -= 1
-                return self.x,self.y
+                return self.x, self.y
             else:
-                return self.x-1, self.y
+                return self.x - 1, self.y
         elif nextmove == "z":
-            if is_empty(self.x,self.y-1):
+            if is_empty(self.x, self.y - 1, map):
                 self.y -= 1
-                return self.x,self.y
+                return self.x, self.y
             else:
-                return self.x, self.y-1
+                return self.x, self.y - 1
         elif nextmove == "s":
-            if is_empty(self.x,self.y-1):
+            if is_empty(self.x, self.y + 1, map):
                 self.y += 1
-                return self.x,self.y
+                return self.x, self.y
             else:
-                return self.x, self.y+1
-    
+                return self.x, self.y + 1
+
     def __repr__(self):
         return f"{self.name} : ({self.x, self.y})"
 
-def is_empty(x,y)
-    if map[y][x] in ['-','|'] :
+
+def is_empty(x, y, map):
+    global file
+    if map[y][x] in ["-", "|", " "]:
         return False
     return True
--- josephine.py
+++ josephine.py
@@ -1,38 +1,36 @@
 from classes import Player
 
-#fonctions utiles
-def print_map(map): 
-    print(''.join([''.join(map[k]) for k in range (len(map))]))
 
-def update_map(player,move,map):
-    with open('josephine.txt','r') as in_file:
-            reader = [list(line) for line in in_file.readlines()]
-            map[player.y][player.x]=reader[player.y][player.x]
-    player.move(move)
-    file[player.y][player.x]='@'
+# fonctions utiles
+def print_map(map):
+    print("".join(["".join(map[k]) for k in range(len(map))]))
 
-#inplémentation de la map
-with open('josephine.txt', 'r') as in_file:
+
+def update_map(player, move, map):
+    with open("josephine.txt", "r") as in_file:
+        reader = [list(line) for line in in_file.readlines()]
+        map[player.y][player.x] = reader[player.y][player.x]
+    player.move(move, map)
+    file[player.y][player.x] = "@"
+
+
+# inplémentation de la map
+with open("josephine.txt", "r") as in_file:
     file = in_file.readlines()
     file = [list(line) for line in file]
 
 ##déroulé du jeu
 
-#initialisation
-#name=input('Quel est ton nom ? ')
-P = Player(1,1,'rogue')
-file[P.y][P.x]='@'
+# initialisation
+# name=input('Quel est ton nom ? ')
+P = Player(1, 1, "rogue")
+file[P.y][P.x] = "@"
 print_map(file)
 
-#jeu en cours
-nextmove=None
-while nextmove!='t':
-    nextmove=input()
-    if nextmove in {'q','s','z','d'}:
-        update_map(P,nextmove,file)
+# jeu en cours
+nextmove = None
+while nextmove != "t":
+    nextmove = input()
+    if nextmove in {"q", "s", "z", "d"}:
+        update_map(P, nextmove, file)
         print_map(file)
-    
-
-
-
-
